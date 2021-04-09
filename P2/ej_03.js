26db09b429593ba57723e97d604484fc83e905c2
RaulRuizSevilla <raulruizs2000@gmail.com>
2021-04-09 18:58:47 +0200

    Ejemplo 3 - javascript script

---
+++ P2/ej_03.js
@@ -0,0 +1,5 @@
+/* Mis primeros comentarios en javascript */
+/* Imprimir 3 mensajes en la consola */
+console.log ("Estoy fuera del HTML");
+console.log ("Se recomienda hacerlo así");
+console.log ("------");
