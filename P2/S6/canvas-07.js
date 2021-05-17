2a4f435e9e2674461ee8073f67d52a01f669e608
RaulRuizSevilla <raulruizs2000@gmail.com>
2021-05-17 20:08:22 +0200

    Ejemplo 7 canvas- poniendo imágenes

--- P2/S6/canvas-07.js
+++ P2/S6/canvas-07.js
@@ -2,20 +2,19 @@ console.log("Ejecutando JS...");
 
 const canvas = document.getElementById("canvas");
 
-/* Definir el tamaño del canvas */
+//-- Definir el tamaño del canvas
 canvas.width = 500;
 canvas.height = 250;
 
-/* Obtener el contexto del canvas */
+//-- Obtener el contexto del canvas
 const ctx = canvas.getContext("2d");
 
-/*  Leer la imagen del documento html
-Esta deshabilitada */
+//-- Leer la imagen del documento html
+//-- Esta deshabilitada
 var logo = document.getElementById("logo-urjc");
 
 logo.onload = ()=> {
- /* Insertar la imagen en el canvas, una vez que
-  ya esté cargada! */
+  //-- Insertar la imagen en el canvas, una vez que
+  //-- ya esté cargada!
   ctx.drawImage(logo, 15,18);
-};
-
+}
