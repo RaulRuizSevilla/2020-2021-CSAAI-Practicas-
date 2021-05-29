87315f29d5c10a1262135c0823d2ea768a8206e3
RaulRuizSevilla <raulruizs2000@gmail.com>
2021-05-29 18:15:30 +0200

    Práctica 5 - ya me funciona todo bien

--- P5/Practica.js
+++ P5/Practica.js
@@ -1,80 +1,87 @@
-/* Configuramos el elemento de vídeo */
-let  directo  =  documento . getElementById ( "directo" ) ;
-const  video1  =  documento . getElementById ( "video1" ) ;
-const  btn_video1  =  documento . getElementById ( "btn_video1" ) ;
-const  video2  =  documento . getElementById ( "video2" ) ;
-const  btn_video2  =  documento . getElementById ( "btn_video2" ) ;
-const  btn_test  =  documento . getElementById ( "btn_test" ) ;
-const  btn_src_on  =  documento . getElementById ( "btn_src_on" ) ;
-const  btn_src_off  =  documento . getElementById ( "btn_src_off" ) ;
+/* Configuración del vídeo */
+let directo = document.getElementById("directo");
+const video1 = document.getElementById("video1");
+const btn_video1 = document.getElementById("btn_video1");
+const video2 = document.getElementById("video2");
+const btn_video2 = document.getElementById("btn_video2");
+const btn_test = document.getElementById("btn_test");
+const btn_src_on = document.getElementById("btn_src_on");
+const btn_src_off = document.getElementById("btn_src_off");
 
 
 /* Dimensiones de los vídeos */
-directo . ancho = 420 ;
-directo . altura = 200 ;
-video1 . ancho = 200 ;  
-video1 . altura = 100 ;
-video2 . ancho = 200 ;  
-video2 . altura = 100 ;
+directo.width=620;
+directo.height=200;
+video1.width=200;  
+video1.height=100;
+video2.width=200;  
+video2.height=100;
 
-/* Imagen de ejemplo test */
-const  TEST_IMAGE_URL  =  "test.png" ;
+/* Imagen TEST */
+const TEST_IMAGE_URL = "test.png";
 
-/* Imagen de antes de que empiece a funcionar */
-directo . poster  =  TEST_IMAGE_URL ;
-video1 . poster  =  TEST_IMAGE_URL ;
-video2 . poster  =  TEST_IMAGE_URL ;
+/* Imagen anterior a arrancar el vídeo */
+directo.poster = TEST_IMAGE_URL;
+video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
 
-/* Botón de fuentes ON */
-btn_src_on . onclick  =  ( )  =>  {
+/* Botón Fuentes ON */
+btn_src_on.onclick = () => {
  
-    /* Fuente de cámara 1 */
-    video1 . src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4" ;
-    /* Reproducción de un vídeo desde 0 */
-    video1 . currentTime  =  0 ;
-    video1 . jugar ( ) ;
-    /* En silencio */
-    video1 . muted ;
-    /* Fuente de cámara 2 */
-    video2 . src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4" ;
-    /* Reproducción de un vídeo desde 0 */
-    video2 . currentTime  =  0 ;
-    video2 . jugar ( ) ;
-    /* En silencio */
-    video2 . muted ;
+    /* Fuente cámara 1 */
+    video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
 
-    /* Imagen de prueba en directo */
-    directo . poster  =  TEST_IMAGE_URL ;
-} ;
+
+    /* Vídeo desde 0 */
+    video1.currentTime = 0;
+    video1.play();
+
+    /* En silencio */
+    video1.muted;
+
+    /* Fuente cámara 2 */
+    video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+
+
+    /* Vídeo desde 0 */
+    video2.currentTime = 0;
+    video2.play();
+
+    /* En silencio */
+    video2.muted;
+
+    /* En directo, imagen de prueba */
+    directo.poster = TEST_IMAGE_URL;
+};
 
 /* Botón TEST */
-btn_test . onclick  =  ( )  =>  {
-    directo . poster  =  TEST_IMAGE_URL ;
-    directo . src  =  null ;
-} ;
+btn_test.onclick = () => {
+    directo.poster = TEST_IMAGE_URL;
+    directo.src = null;
+};
 
-/* Botón cámara 1 */
-btn_video1 . onclick  =  ( )  =>  {
-    directo . src  =  video1 . src ;
-    directo . currentTime  =  video1 . currentTime ;
-    directo . jugar ( ) ;
-    directo . cartel = null ;
-} ;
+/* Botón cámara 1*/
+btn_video1.onclick = () => {
+    directo.src = video1.src;
+    directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
 
-/* Botón Cámara 2 */
-btn_video2 . onclick  =  ( )  =>  {
-    directo . src  =  video2 . src ;
-    directo . currentTime  =  video2 . currentTime ;
-    directo . jugar ( ) ;
-    directo . cartel = null ;
-} ;
+/* Botón cámara 2 */
+btn_video2.onclick = () => {
+    directo.src = video2.src;
+    directo.currentTime = video2.currentTime;
+    directo.play();
+    directo.poster=null;
+};
 
-/* Botón SRC desactivado */
-btn_src_off . onclick  =  ( )  =>  {
-    video1 . poster  =  TEST_IMAGE_URL ;
-    video2 . poster  =  TEST_IMAGE_URL ;
-    video1 . src  =  null ;
-    video2 . src  =  null ;
-    directo . poster  =  TEST_IMAGE_URL ;
-    directo . src  =  null ;
+ /* Botón SRC */
+btn_src_off.onclick = () => {
+    video1.poster = TEST_IMAGE_URL;
+    video2.poster = TEST_IMAGE_URL;
+    video1.src = null;
+    video2.src = null;
+    directo.poster = TEST_IMAGE_URL;
+    directo.src = null;
 }
